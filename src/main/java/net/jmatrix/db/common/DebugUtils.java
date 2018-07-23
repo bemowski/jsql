823e8c3f19d4262c634b098b6274c152db258a28
Paul Bemowski <bemowski@gmail.com>
2018-07-23 18:19:09 -0500

    fudge

--- src/main/java/net/jmatrix/db/common/DebugUtils.java
+++ src/main/java/net/jmatrix/db/common/DebugUtils.java
@@ -107,7 +107,7 @@ public class DebugUtils {
          return "null";
       
       if (s.length() > len) {
-         return s.substring(0, len)+"...";
+         return s.substring(0, len-3)+"...";
       }
       return s;
    }
